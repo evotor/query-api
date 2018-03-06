11b577424065119f8a323779efaf9232450bd126
Луньков Артем Дмитриевич <a.lunkov@evotor.local>
2018-03-06 13:23:29 +0300

    init

---
+++ src/main/java/ru/evotor/Cursor.kt
@@ -0,0 +1,12 @@
+package ru.evotor
+
+import android.database.Cursor
+
+
+abstract class Cursor<out T>(cursor: Cursor) : android.database.Cursor by cursor {
+
+    abstract fun getValue(): T
+
+    abstract fun toList(): List<T>?
+
+}
---
+++ src/main/java/ru/evotor/FieldFilter.kt
@@ -0,0 +1,75 @@
+package ru.evotor
+
+/**
+ * Created by a.lunkov on 27.02.2018.
+ */
+
+abstract class FieldFilter<V, Q, S : FilterBuilder.SortOrder<S>, T>(private val fieldName: String) {
+
+    fun equal(value: V?): Executor<Q, S, T> {
+        return equalNotEqual("=", value)
+    }
+
+    fun notEqual(value: V?): Executor<Q, S, T> {
+        return equalNotEqual("!=", value)
+    }
+
+    private fun equalNotEqual(operator: String, value: V?): Executor<Q, S, T> {
+        var result = fieldName + operator
+        result += value?.toString() ?: "null"
+        return appendResult(result)
+    }
+
+    fun greater(value: V, including: Boolean): Executor<Q, S, T> {
+        return greaterLower('>', value, including)
+    }
+
+    fun lower(value: V, including: Boolean): Executor<Q, S, T> {
+        return greaterLower('<', value, including)
+    }
+
+    private fun greaterLower(operator: Char, value: V, including: Boolean): Executor<Q, S, T> {
+        var result = "$fieldName$operator"
+        if (including) {
+            result += "="
+        }
+        result += value
+        return appendResult(result)
+    }
+
+    fun like(text: String): Executor<Q, S, T> {
+        return appendResult("$fieldName LIKE $text")
+    }
+
+    fun between(leftValue: V, rightValue: V): Executor<Q, S, T> {
+        return appendResult("$fieldName BETWEEN $leftValue AND $rightValue")
+    }
+
+    fun inside(values: List<V>): Executor<Q, S, T> {
+        return insideNotInside("IN", values)
+    }
+
+    fun inside(values: Array<V>): Executor<Q, S, T> {
+        return inside(values.toList())
+    }
+
+    fun notInside(values: List<V>): Executor<Q, S, T> {
+        return insideNotInside("NOT IN", values)
+    }
+
+    fun notInside(values: Array<V>): Executor<Q, S, T> {
+        return notInside(values.toList())
+    }
+
+    private fun insideNotInside(operator: String, values: List<V>): Executor<Q, S, T> {
+        val result = StringBuilder()
+        values.forEach {
+            result.append("$it,")
+        }
+        result.setCharAt(result.length - 1, ')')
+        return appendResult("$fieldName $operator ($result")
+    }
+
+    protected abstract fun appendResult(edition: String): Executor<Q, S, T>
+
+}
